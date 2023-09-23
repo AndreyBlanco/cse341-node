1441aadcf3d39e9f3d1d59ab4861aaab1ed7a864
andreyblanco <andygblanco@gmail.com>
2023-09-23 13:56:16 -0600

    clean

--- Week02/routes/index.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const routes = require('express').Router();
-
-const myControllers = require('../controllers');
-
-routes.get('/', myControllers.getData);
-
-module.exports = routes;
