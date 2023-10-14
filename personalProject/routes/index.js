96adfa19d04a3651a2036a1ee230880f43591830
andreyblanco <andygblanco@gmail.com>
2023-10-14 17:32:27 -0600

    clear this week

--- personalProject/routes/index.js
+++
@@ -1,9 +0,0 @@
-const swagger = require('./swagger');
-const express = require('express');
-const router = express.Router();
-
-router.use('/', swagger);
-router.use('/students', require('./students'));
-router.use('/disabilities', require('./disabilities'));
-
-module.exports = router;
