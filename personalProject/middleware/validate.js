67fe424519efa771c9868c49bc8e1f6224b13698
andreyblanco <andygblanco@gmail.com>
2023-10-29 08:45:55 -0600

    password validation

--- personalProject/middleware/validate.js
+++ personalProject/middleware/validate.js
@@ -45,22 +45,64 @@ const saveTeacher = (req, res, next) => {
   const validationRule = {
     firstName: 'required|string',
     lastName: 'required|string',
-    email: 'required|email',
-    password: 'required|password8'
+    email: 'required|email'/*,
+    password: 'required|min:8'*/
   };
   validator(req.body, validationRule, {}, (err, status) => {
-    if (!status) {
-      res.status(412).send({
-        success: false,
-        message: 'Validation failed',
-        data: err
-      });
+    var passpatt=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    var passStatus = false;
+    var passError = [
+      "The password should be at least 8 characters long.",
+      "The password should contain at least one uppercase letter.",
+      "The password should contain one lowercase leter.",
+      "The password should contain one number."
+    ];
+
+    if (req.body.password.match(passpatt)) {
+      passStatus = true;
+      if (!status) {
+        res.status(412).send({
+          success: false,
+          message: 'Validation failed' + req.body.password,
+          data: err
+        });
+      } else {
+        next();
+      }
     } else {
-      next();
+      passStatus = false;
+      if (!status) {
+        err.errors["password"]=passError;
+        res.status(412).send({
+          success: false,
+          message: 'Validation failed' + req.body.password,
+          data: err
+        });
+      } else {
+        var terr = {
+          errors: [
+            {
+            "password": passError
+            }
+          ]
+        };
+        
+        res.status(412).send({
+          success: false,
+          message: 'Validation failed' + req.body.password,
+          data: terr
+        });
+      }
     }
-  });
-};
+    
+});
+
+}
 
 module.exports = {
   saveStudent, saveDisability, saveTeacher
 };
+
+/*password8: function (val) {
+  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(val);
+},*/
