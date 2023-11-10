ad7abb1c8587531666c42e08b28192d2401855cb
andreyblanco <andygblanco@gmail.com>
2023-11-10 10:36:37 -0600

    login

--- personalProject/routes/login.js
+++ personalProject/routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 const config = {
     authRequired: false,
@@ -14,5 +14,7 @@ const config = {
 router.use(auth(config));
 
 router.get('/', (req, res) => {res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');});
+
+router.get('/profile', requiresAuth(), (req, res) =>{res.send(JSON.stringify(req.oidc.user));});
 
 module.exports = router;
