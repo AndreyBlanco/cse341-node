d168fc2d49575e9e129fe7234fbc2191c0fae8a5
andreyblanco <andygblanco@gmail.com>
2023-11-10 13:44:20 -0600

    changes

--- personalProject/index.ts
+++ personalProject/index.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+const mongodb = require('./db/connect.ts');
 const Routes = require('./routes');
 const { auth, requiresAuth } = require('express-openid-connect');
 
