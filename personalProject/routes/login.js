d3df6047d5406dd4315f3afb931f651a432c3b17
andreyblanco <andygblanco@gmail.com>
2023-11-10 10:30:43 -0600

    update login

--- personalProject/routes/login.js
+++ personalProject/routes/login.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { auth } = require('express-openid-connect');
 
-const config = auth({
+const config = {
     authRequired: false,
     auth0Logout: true,
     secret: process.env.SECRET,
     baseURL: process.env.BASE_URL,
     clientID: process.env.CLIENT_ID,
     issuerBaseURL: process.env.ISSUER_BASE_URL,
-});
+};
 
 router.use(auth(config));
 
